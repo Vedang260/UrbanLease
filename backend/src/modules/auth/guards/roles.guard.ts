91c471db4f8188ac0bc5417649b4af0fab59a8d4
Vedang <vedang.wappnet@gmail.com>
2025-05-14 11:33:16 +0530

    added roles guard

---
+++ backend/src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '../../../common/enums/roles.enums';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    
+    if (!requiredRoles) {
+      return true;
+    }
+    
+    const { user } = context.switchToHttp().getRequest();
+    console.log('User from request:', user);
+    if (typeof user.role === 'string') {
+      return requiredRoles.includes(user.role);
+    }
+  
+    // If user.role is an array (multiple roles), check if any match
+    if (Array.isArray(user.role)) {
+      return user.role.some((role) => requiredRoles.includes(role));
+    }
+
+    return false;
+  }
+} 
