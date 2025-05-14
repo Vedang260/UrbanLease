579260ec78bee45d36d3f7e0ab40298c1123a378
Vedang <vedang.wappnet@gmail.com>
2025-05-14 18:23:09 +0530

    added create address dto

---
+++ backend/src/modules/properties/dtos/createAddress.dto.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateAddressDto{
+    @IsNotEmpty()
+    @IsString()
+    street: string;
+
+    @IsNotEmpty()
+    @IsString()
+    city: string;
+
+    @IsNotEmpty()
+    @IsString()
+    state: string;
+
+    @IsNotEmpty()
+    @IsString()
+    country: string;
+
+    @IsNotEmpty()
+    @IsString()
+    zipcode: string;
+}
