110128e28de119423666b57bebf86472eda1ec4a
Vedang <vedang.wappnet@gmail.com>
2025-05-14 17:20:34 +0530

    propertyType is added

---
+++ backend/src/common/enums/propertyType.enums.ts
@@ -0,0 +1,14 @@
+export enum PropertyType {
+  APARTMENT = 'apartment',
+  INDEPENDENT_HOUSE = 'independent_house',
+  VILLA = 'villa',
+  PG = 'pg',
+  SHARED_ROOM = 'shared_room',
+  BUNGALOW = 'bungalow',
+  FARMHOUSE = 'farmhouse',
+  COMMERCIAL_SHOP = 'commercial_shop',
+  OFFICE_SPACE = 'office_space',
+  WAREHOUSE = 'warehouse',
+  PLOT = 'plot',
+  HOSTEL = 'hostel'
+}
