91bb0447ca81daa64bdfa0d1f339603e80b57fde
Vedang <vedang.wappnet@gmail.com>
2025-05-14 17:47:59 +0530

    added location entity

--- backend/src/modules/properties/entities/location.entity.ts
+++ backend/src/modules/properties/entities/location.entity.ts
@@ -1,5 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: 'locations'})
 export class Location{
+
+    @PrimaryGeneratedColumn('uuid')
     locationId: string;
+
+    @Column()
     latitude: number;
+
+    @Column()
     longitude: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+    
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
