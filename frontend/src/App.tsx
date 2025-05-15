0b56c8a1e8e18364c3fca9216c15a2ec9f7841ac
Vedang <vedang.wappnet@gmail.com>
2025-05-15 12:54:03 +0530

    app set-up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/core/Navbar';
 import Footer from './components/core/Footer';
+import AppRoutes from './routes';
 
 function App() {
-
-  return (  
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow">
-        {/* Your page content goes here */}
-      </main>
-      <Footer />
-    </div>
-  )
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
