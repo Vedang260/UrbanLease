78c8ed396cbb904b595339c815dfb79e3d48ca64
Vedang <vedang.wappnet@gmail.com>
2025-05-16 11:37:58 +0530

    property repository is created

--- backend/src/modules/properties/repositories/property.repository.ts
+++ backend/src/modules/properties/repositories/property.repository.ts
@@ -4,6 +4,7 @@ import { Property } from "../entities/property.entity";
 import { Repository } from "typeorm";
 import { CreatePropertyDto } from "../dtos/createProperty.dto";
 import { updatePropertyDto } from "../dtos/updateProperty.dto";
+import { PropertyStatus } from "src/common/enums/propertyStatus.enums";
 
 @Injectable()
 export class PropertyRepository{
@@ -42,12 +43,66 @@ export class PropertyRepository{
         }
     }
 
-    async getPropertyRequests(){
-        try{
+    async getPropertyRequests(): Promise<Property[]> {
+        try {
+            const properties = await this.propertyRepository.find({
+                relations: [
+                    'owner',
+                    'location',
+                    'address',
+                    'features',
+                ],
+                order: {
+                    createdAt: 'DESC',
+                },
+                where: { status: PropertyStatus.PENDING_APPROVAL }
+            });
 
+            return properties;
+        } catch (error) {
+            console.error('Error in fetching property requests: ', error.message);
+            throw new InternalServerErrorException('Error in fetching property requests');
+        }
+    }
+
+    async getPropertyListing(){
+        try{
+            const properties = await this.propertyRepository.find({
+                relations: [
+                    'owner',
+                    'location',
+                    'address',
+                    'features',
+                ],
+                order: {
+                    createdAt: 'DESC',
+                },
+                where: { status: PropertyStatus.AVAILABLE }
+            });
+
+            return properties;
         }catch(error){
             console.error('Error in fetching property requests: ', error.message);
-            throw new InternalServerErrorException('Error in fetching property requests ')
+            throw new InternalServerErrorException('Error in fetching property requests');
+        }
+    }
+
+    async getOwnerProperties(){
+        try{
+            const properties = await this.propertyRepository.find({
+                relations: [
+                    'owner',
+                    'location',
+                    'address',
+                    'features',
+                ],
+                order: {
+                    createdAt: 'DESC',
+                },
+            });
+        }catch(error){
+            console.error('Error in fetching owner properties: ', error.message);
+            throw new InternalServerErrorException('Error in fetching owner properties');
         }
     }
 }
