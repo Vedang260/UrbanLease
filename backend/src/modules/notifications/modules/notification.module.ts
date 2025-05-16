861d464b866ca7215bbc8df387d6dfdb9e047fff
Vedang <vedang.wappnet@gmail.com>
2025-05-16 11:03:57 +0530

    imported processor

--- backend/src/modules/notifications/modules/notification.module.ts
+++ backend/src/modules/notifications/modules/notification.module.ts
@@ -4,11 +4,12 @@ import { Notification } from '../entities/notification.entity';
 import { NotificationController } from '../controllers/notification.controller';
 import { NotificationService } from '../services/notification.service';
 import { NotificationRepository } from '../repositories/notification.repository';
+import { NotificationProcessor } from '../processor/notification.processor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification])],
   controllers: [NotificationController],
-  providers: [NotificationService, NotificationRepository],
-  exports: [NotificationService, NotificationRepository],
+  providers: [NotificationService, NotificationRepository, NotificationProcessor],
+  exports: [NotificationService, NotificationRepository, NotificationProcessor],
 })
 export class NotificationModule {} 
