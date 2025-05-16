95eef5b5877fcd5741ab1f804845824b6f599b79
Vedang <vedang.wappnet@gmail.com>
2025-05-16 10:51:58 +0530

    added webocket gateway for notification

---
+++ backend/src/modules/notifications/gateway/notification.gateway.ts
@@ -0,0 +1,17 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { Notification } from '../entities/notification.entity';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*', // Allow requests from any frontend
+  },
+})
+export class WebsocketGateway {
+  @WebSocketServer()
+  server: Server;
+
+  sendNotificationAlert(newNotification: Notification) {
+    this.server.emit('notificationAlert', {newNotification});
+  }
+}
