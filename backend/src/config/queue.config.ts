b2be1d825841b1e245045a63a51461ebf49bfc9c
Vedang <vedang.wappnet@gmail.com>
2025-05-16 10:14:04 +0530

    added queue config

---
+++ backend/src/config/queue.config.ts
@@ -0,0 +1,13 @@
+// src/config/queue.config.ts
+import { BullModuleOptions } from '@nestjs/bull';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
+export const bullConfig = (config: ConfigService): BullModuleOptions => ({
+  redis: {
+    host: process.env.REDIS_HOST,       // Upstash Redis URL
+    port: Number(process.env.REDIS_PORT),       // Upstash Redis port
+    password: process.env.REDIS_PASSWORD, // Upstash Redis password
+    tls: {},                              // Required for Upstash (SSL)
+  },
+});
