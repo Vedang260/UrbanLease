17e980985b4fbf31a3db591e743457d900858d21
Vedang <vedang.wappnet@gmail.com>
2025-05-17 09:08:10 +0530

    added redux

---
+++ frontend/src/redux/slice/authSlice.ts
@@ -0,0 +1,39 @@
+// features/auth/authSlice.ts
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthState {
+  user: {
+    id: string;
+    username: string;
+    email: string;
+    role: string;
+  } | null;
+  token: string | null;
+  isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginSuccess(state, action: PayloadAction<{ user: any; token: string }>) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
