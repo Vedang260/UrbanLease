fd4f16e5ed960a1ca31764fa49bca21465fd2e9a
Vedang <vedang.wappnet@gmail.com>
2025-05-17 09:20:03 +0530

    added redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { LoadingSpinner } from './components/LoadingAnimation.tsx';
+import { persistor, store } from './redux/store/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+const Root = () => (
+  <Provider store={store}>
+    <PersistGate loading={<LoadingSpinner />} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
